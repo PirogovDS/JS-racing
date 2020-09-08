fe66496075c0819ac947aeae84a4c5a97a52cbf7
Dmitry <64146898+PirogovDS@users.noreply.github.com>
2020-09-08 19:58:43 +0300

    lesson 1

---
+++ main.js
@@ -0,0 +1,47 @@
+const score = document.querySelector('.score'),
+    start = document.querySelector('.start'),
+    gamearea = document.querySelector('.gamearea'),
+    car = document.createElement('div');
+
+car.classList.add('car');
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+const keys = {
+    ArrowUp: false,
+    ArrowDwn: false,
+    ArrowRight: false,
+    ArrowLeft: false
+};
+
+const setting = {
+    start: false,
+    score: 0,
+    speed: 3
+};
+
+function startGame() {
+    start.classList.add('hide');
+    setting.start = true;
+    gamearea.appendChild(car);
+    requestAnimationFrame(playGame);
+};
+
+function playGame() {
+    console.log('Play game!');
+    if (setting.start) {
+        requestAnimationFrame(playGame);
+    };
+};
+
+function startRun(event) {
+    event.preventDefault();
+    keys[event.key] = true;
+};
+
+function stopRun(event) {
+    event.preventDefault();
+    keys[event.key] = false;
+};
